c6ac569fadd1903f8f194eec2c1092ee766a8f14
Rayhan Shorker <raihanshorker56@gmail.com>
2023-06-04 17:45:40 +0600

    initial setup

---
+++ src/Pages/TaskList/TaskList.jsx
@@ -0,0 +1,11 @@
+
+
+const TaskList = () => {
+    return (
+        <div>
+            <h3>task list</h3>
+        </div>
+    );
+};
+
+export default TaskList;
---
+++ src/Pages/TaskManagement/TaskManagement.jsx
@@ -0,0 +1,11 @@
+
+
+const TaskManagement = () => {
+    return (
+        <div>
+            <h3>manage task</h3>
+        </div>
+    );
+};
+
+export default TaskManagement;
